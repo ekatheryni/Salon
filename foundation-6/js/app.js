d3728a8b643d02e73ff2f9afb5a034ead46bcc22
MMatveieva <matveieva.mariya@gmail.com>
2018-06-03 16:36:53 +0300

    top bar edit

--- foundation-6/js/app.js
+++ foundation-6/js/app.js
@@ -1,1 +1,35 @@
-$(document).foundation()
+$(function () {
+    $(document).foundation();
+
+    $('.about-click').click(function () {
+        $('html,body').animate({
+                scrollTop: $('.main-info').offset().top - 90
+            },
+            'slow');
+    });
+    $('.services-click').click(function () {
+        $('html,body').animate({
+                scrollTop: $('.services').offset().top - 90
+            },
+            'slow');
+    });
+    $('.photo-click').click(function () {
+        $('html,body').animate({
+                scrollTop: $('.gallery').offset().top - 90
+            },
+            'slow');
+    });
+    $('.contacts-click').click(function () {
+        $('html,body').animate({
+                scrollTop: $('.contacts').offset().top - 90
+            },
+            'slow');
+    });
+    $('.res-click').click(function () {
+        $('html,body').animate({
+                scrollTop: $('.results').offset().top - 90
+            },
+            'slow');
+    });
+
+});
